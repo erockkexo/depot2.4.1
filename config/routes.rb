d1da5a17d493eeb14f432726d45fd005b1b00aaa
erockk <imac@imacs-iMac.local>
2018-12-29 10:35:41 -0500

    Added Caching

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'store/index'
-
-  root 'store#index', as: 'store_index'
-
+  root to: 'store#index'
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
