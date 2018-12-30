07f3a6e265c2188511abcd37df72444dab5b4165
erockk <imac@imacs-iMac.local>
2018-12-29 19:30:47 -0500

    finished cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,18 @@
 class StoreController < ApplicationController
-  def index
-  	@products = Product.order(:title)
+
+
+  def counter
+  	if session[:counter].nil?
+  		session[:counter] = 0
+  	end
+  	session[:counter] += 1
   end
+
+  def index
+    @products = Product.order(:title)
+    @count = counter
+    @message = "You have been here #{@count} times" if @count > 5
+
+  end
+
 end
