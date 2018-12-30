1a1344ee41f00720a23998ff96ad17d7a6f17527
erockk <imac@imacs-iMac.local>
2018-12-30 15:55:09 -0500

    Minor changes

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -57,6 +57,13 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
 
+  def destroy
+    @line_item.destroy
+    respond_to do |format|
+      format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
 
 
   private
