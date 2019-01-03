dc6203eeb41dac7349081541be47eb77a060a6fd
erockk <imac@imacs-iMac.local>
2019-01-03 08:37:17 -0500

    added checkout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items
   resources :carts
   root to: 'store#index'
-  resources :product
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :products do
+    get :who_bought, on: :member
+  end
 
 
 end
